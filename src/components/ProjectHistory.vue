ee3db143f00e161591f377bcfd1ee33496432314
Asea <569389750@qq.com>
2023-01-08 19:46:54 +0800

    feat: init

---
+++ src/components/ProjectHistory.vue
@@ -0,0 +1,81 @@
+<template>
+  <el-dialog
+    v-model="store.state.model.historyModelFlag"
+    title="更新记录"
+    :width="store.state.model.modelWidth"
+  >
+    <div id="tree" style="height: 60vh; overflow: auto">
+      <el-timeline>
+        <el-timeline-item
+          v-for="(activity, index) in activities"
+          :key="index"
+          :icon="activity.icon"
+          :type="activity.type"
+          :color="activity.color"
+          :size="activity.size"
+          :hollow="activity.hollow"
+          :timestamp="activity.timestamp"
+        >
+          {{ activity.content }}
+        </el-timeline-item>
+      </el-timeline>
+    </div>
+  </el-dialog>
+</template>
+
+<script setup>
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const activities = [
+  {
+    content: "增加总金额，添加滚动数字动画，增加快捷键Alt+Q，以及回车监听",
+    timestamp: "2023-1-8",
+    size: "large",
+    type: "primary",
+  },
+  {
+    content: "修复日期排序问题、优化动画",
+    timestamp: "2023-1-3",
+    size: "large",
+    type: "primary",
+  },
+  {
+    content: "完善主题，第一版正式上线",
+    timestamp: "2022-12-29",
+    size: "large",
+    type: "primary",
+  },
+  {
+    content: "写了一堆",
+    timestamp: "2022-12-29",
+    size: "large",
+    type: "primary",
+  },
+  {
+    content: "初始化",
+    timestamp: "2022-12-26",
+    size: "large",
+    type: "primary",
+  },
+];
+</script>
+
+<style scoped>
+/*滚动条样式*/
+#tree::-webkit-scrollbar {
+  width: 6px;
+  /*height: 4px;*/
+}
+#tree::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  /* -webkit-box-shadow: inset 0 0 5px rgba(145, 145, 145, 0.2); */
+  background: rgba(147, 147, 147, 0.2);
+}
+#tree::-webkit-scrollbar-track {
+  /* -webkit-box-shadow: inset 0 0 5px rgba(149, 149, 149, 0.2); */
+  border-radius: 0;
+  background: rgba(174, 174, 174, 0.1);
+}
+</style>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="topnav">
+    <div class="left">
+      <!-- 账户 -->
+      <span
+        class="header baseline"
+        @click="openLoginModel"
+        v-if="!store.state.user.isLogin"
+      >
+        登录
+      </span>
+      <el-popconfirm title="退出登录？" @confirm="emit('logout')" v-else>
+        <template #reference>
+          <span class="header baseline">
+            {{ store.state.user.nickName }}
+          </span>
+        </template>
+      </el-popconfirm>
+    </div>
+    <div class="right">
+      <!-- 主题 -->
+      <span class="icon" @click="changeTheme">
+        <el-icon>
+          <Sunrise v-if="store.state.settings.theme !== 'light'" />
+          <MoonNight v-else />
+        </el-icon>
+      </span>
+
+      <!-- 更新记录 -->
+      <span class="history icon" @click="openHisory">
+        <el-icon><Clock /></el-icon>
+      </span>
+      <!-- 设置 -->
+      <span
+        class="settings icon"
+        @click="
+          store.state.model.settingsModelFlag =
+            !store.state.model.settingsModelFlag
+        "
+      >
+        <el-icon><Setting /></el-icon>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {
+  Clock,
+  Setting,
+  Sunny,
+  Sunrise,
+  MoonNight,
+} from "@element-plus/icons-vue";
+import { useStore } from "vuex";
+
+const emit = defineEmits(["logout"]);
+const store = useStore();
+
+const changeTheme = () => {
+  const newTheme = store.state.settings.theme === "light" ? "dark" : "light";
+  document.documentElement.className = newTheme;
+  store.commit("updateSettings", {
+    key: "theme",
+    value: (store.state.settings.theme = newTheme),
+  });
+};
+document.documentElement.className = store.state.settings.theme;
+
+// 登录登出
+function openLoginModel() {
+  if (!store.state.user.isLogin) {
+    store.state.model.loginModelFlag = !store.state.model.loginModelFlag;
+  }
+}
+
+function openHisory() {
+  store.commit("updateModel", { key: "historyModelFlag", value: true });
+}
+</script>
+
+<style scoped lang="scss">
+.topnav {
+  width: 100%;
+  // position: fixed;
+  top: 0;
+  z-index: 20;
+  background: radial-gradient(transparent 1px, var(--bg-transparant-color) 1px);
+  backdrop-filter: saturate(100%) blur(10px);
+  height: 50px;
+}
+.left {
+  position: absolute;
+  left: 20px;
+  top: 12px;
+  display: flex;
+  gap: 10px;
+}
+.right {
+  position: absolute;
+  right: 20px;
+  top: 12px;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+}
+.header {
+  cursor: pointer;
+  font-size: 1.2em;
+  font-weight: bold;
+}
+.icon {
+  font-size: 25px;
+  transition: transform 0.3s;
+  cursor: pointer;
+}
+
+.history,
+.settings {
+  .el-icon {
+    transition: 0.3s;
+  }
+  .el-icon:hover {
+    transform: rotate(180deg);
+  }
+}
+.baseline::after {
+  content: "";
+  display: block;
+  height: 2px;
+  width: 0%;
+  margin-top: 3px;
+  background: var(--font-color);
+  transition: 0.3s;
+}
+.baseline:hover::after {
+  width: 100%;
+}
+</style>
